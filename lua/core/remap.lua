6d4b33d8a668dfa6a6c08d1f23d163c516549861
fbiondi <federico.biondi@rms.com>
2023-11-02 22:13:44 -0300

    tmux session keymap

--- lua/core/remap.lua
+++ lua/core/remap.lua
@@ -30,6 +30,8 @@ vim.keymap.set("i", "<C-c>", "<Esc>")
 
 vim.keymap.set("n", "Q", "<nop>")
 
+vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+
 -- quickfix navigation
 vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
 vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
