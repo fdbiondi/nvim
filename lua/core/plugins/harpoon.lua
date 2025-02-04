10d5faf86e8e6dc1acfa9bad3edd4dbd8bb7e30b
fbiondi <fdbion@gmail.com>
2025-02-04 14:28:29 -0300

    harpoon updated

--- lua/core/plugins/harpoon.lua
+++ lua/core/plugins/harpoon.lua
@@ -22,5 +22,21 @@ return {
         vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
         vim.keymap.set("n", "<C-l>", function() harpoon:list():select(2) end)
         vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
+
+        harpoon:extend({
+            UI_CREATE = function(cx)
+                vim.keymap.set("n", "<C-v>", function()
+                    harpoon.ui:select_menu_item({ vsplit = true })
+                end, { buffer = cx.bufnr })
+
+                vim.keymap.set("n", "<C-x>", function()
+                    harpoon.ui:select_menu_item({ split = true })
+                end, { buffer = cx.bufnr })
+
+                vim.keymap.set("n", "<C-t>", function()
+                    harpoon.ui:select_menu_item({ tabedit = true })
+                end, { buffer = cx.bufnr })
+            end,
+        })
     end
 }
