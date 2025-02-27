80671fc953cc42a5056ea5c0d93fdc145fed47c9
fbiondi <fdbion@gmail.com>
2025-02-27 11:09:20 -0300

    fix todo comments config

--- lua/core/plugins/comments.lua
+++ lua/core/plugins/comments.lua
@@ -58,6 +58,8 @@ return {
         },
 
         config = function()
+            require('todo-comments').setup()
+
             vim.keymap.set("n", "]t", function()
                 require("todo-comments").jump_next()
             end, { desc = "Next todo comment" })
