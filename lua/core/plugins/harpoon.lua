2964d9a1b25145f01bcee6019d28518ace05d7df
Federico Biondi <fdbion@gmail.com>
2025-05-28 12:19:58 -0300

    lazy update

--- lua/core/plugins/harpoon.lua
+++ lua/core/plugins/harpoon.lua
@@ -1,42 +1,44 @@
 return {
-    "ThePrimeagen/harpoon",
-    branch = "harpoon2",
-    dependencies = { "nvim-lua/plenary.nvim" },
+    {
+        "ThePrimeagen/harpoon",
+        branch = "harpoon2",
+        dependencies = { "nvim-lua/plenary.nvim" },
 
-    config = function()
-        local harpoon = require("harpoon")
-        harpoon:setup({
-            settings = {
-                save_on_toggle = true,
-                sync_on_ui_close = false,
-                key = function()
-                    return vim.loop.cwd()
+        config = function()
+            local harpoon = require("harpoon")
+            harpoon:setup({
+                settings = {
+                    save_on_toggle = true,
+                    sync_on_ui_close = false,
+                    key = function()
+                        return vim.loop.cwd()
+                    end,
+                },
+            })
+
+            vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end, { desc = "Add file to harpoon" })
+            vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end,
+                { desc = "Toggle harpoon menu" })
+
+            vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
+            vim.keymap.set("n", "<C-l>", function() harpoon:list():select(2) end)
+            vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
+
+            harpoon:extend({
+                UI_CREATE = function(cx)
+                    vim.keymap.set("n", "<C-v>", function()
+                        harpoon.ui:select_menu_item({ vsplit = true })
+                    end, { buffer = cx.bufnr })
+
+                    vim.keymap.set("n", "<C-x>", function()
+                        harpoon.ui:select_menu_item({ split = true })
+                    end, { buffer = cx.bufnr })
+
+                    vim.keymap.set("n", "<C-t>", function()
+                        harpoon.ui:select_menu_item({ tabedit = true })
+                    end, { buffer = cx.bufnr })
                 end,
-            },
-        })
-
-        vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end, { desc = "Add file to harpoon" })
-        vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end,
-            { desc = "Toggle harpoon menu" })
-
-        vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
-        vim.keymap.set("n", "<C-l>", function() harpoon:list():select(2) end)
-        vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
-
-        harpoon:extend({
-            UI_CREATE = function(cx)
-                vim.keymap.set("n", "<C-v>", function()
-                    harpoon.ui:select_menu_item({ vsplit = true })
-                end, { buffer = cx.bufnr })
-
-                vim.keymap.set("n", "<C-x>", function()
-                    harpoon.ui:select_menu_item({ split = true })
-                end, { buffer = cx.bufnr })
-
-                vim.keymap.set("n", "<C-t>", function()
-                    harpoon.ui:select_menu_item({ tabedit = true })
-                end, { buffer = cx.bufnr })
-            end,
-        })
-    end
+            })
+        end
+    }
 }
