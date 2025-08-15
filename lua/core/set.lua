4b698027803bc877ae3847351d046f1b1ea33975
Federico Biondi <fdbion@gmail.com>
2025-08-15 10:06:48 -0300

    Revert "use shorthand"
    This reverts commit 9871e75acb72fadcdeda96b7d9908a6ee233586c.

--- lua/core/set.lua
+++ lua/core/set.lua
@@ -1,31 +1,31 @@
-vim.o.guicursor = ""
+vim.opt.guicursor = ""
 
-vim.o.nu = true
-vim.o.relativenumber = true
+vim.opt.nu = true
+vim.opt.relativenumber = true
 
-vim.o.tabstop = 4
-vim.o.softtabstop = 4
-vim.o.shiftwidth = 4
-vim.o.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
 
-vim.o.smartindent = true
+vim.opt.smartindent = true
 
-vim.o.wrap = false
+vim.opt.wrap = false
 
-vim.o.swapfile = false
-vim.o.backup = false
-vim.o.undodir = os.getenv("HOME") .. "/.vim/undodir"
-vim.o.undofile = true
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
 
-vim.o.hlsearch = false
-vim.o.incsearch = true
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
 
-vim.o.termguicolors = true
+vim.opt.termguicolors = true
 
-vim.o.scrolloff = 8
-vim.o.signcolumn = "yes"
-vim.o.isfname:append("@-@")
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
 
-vim.o.updatetime = 50
+vim.opt.updatetime = 50
 
-vim.o.colorcolumn = "80"
+vim.opt.colorcolumn = "80"
