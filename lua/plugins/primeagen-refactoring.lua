3fa9f873e92de68e44718bf6560f7265939034ce
Federico Biondi <fdbion@gmail.com>
2026-04-22 13:21:31 -0300

    fix dependency on refactoring plugin

--- lua/plugins/primeagen-refactoring.lua
+++ lua/plugins/primeagen-refactoring.lua
@@ -2,6 +2,7 @@ return {
     "ThePrimeagen/refactoring.nvim",
 
     dependencies = {
+        "lewis6991/async.nvim",
         "nvim-lua/plenary.nvim",
         "nvim-treesitter/nvim-treesitter",
     },
